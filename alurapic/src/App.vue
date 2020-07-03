71277251ea1ec2507c32f64963be201b9507b58b
farleybsd <farley.t.i@hotmail.com>
2020-07-03 09:49:51 -0300

    Filtrando efetivamente a lista

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -7,9 +7,9 @@
       v-on:input="filtro=$event.target.value"
       placeholder="filtre por parte do titulo"
     />
-    {{filtro}}
+
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <img class="imagem-responsiva" :src="foto.url" v-bind:alt="foto.titulo" />
         </meu-painel>
@@ -31,6 +31,20 @@ export default {
       filtro: ""
     };
   },
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        // filtra a lista, por enquanto vamos retornar uma lista em branco
+        let exp = new RegExp(this.filtro.trim(), "i");
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+        return [];
+      } else {
+        // se o campo estiver vazio, não filtramos, retornamos a lista
+        return this.fotos;
+      }
+    }
+  },
+
   created() {
     // alert("Criei o component");
     let promise = this.$http.get("http://localhost:3000/v1/fotos");
