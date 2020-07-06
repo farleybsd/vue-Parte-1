5422db4d9e12f1be5398f613868becf08dc0e58a
farleybsd <farley.t.i@hotmail.com>
2020-07-06 14:41:29 -0300

    Ajustando a aplicação para SPA

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,60 +1,9 @@
 <template>
-  <div class="corpo">
-    <h1 class="centralizado">{{titulo}}</h1>
-    <input
-      type="search"
-      class="filtro"
-      v-on:input="filtro=$event.target.value"
-      placeholder="filtre por parte do titulo"
-    />
-
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
-        <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
-        </meu-painel>
-      </li>
-    </ul>
-  </div>
+  <div class="corpo"></div>
 </template>
 
 <script>
-import Painel from "./components/shared/painel/Paniel.vue";
-import imagemResponsiva from "./components/shared/imagem-responsiva/imagemResponsiva.vue";
-export default {
-  components: {
-    "meu-painel": Painel,
-    "imagem-responsiva": imagemResponsiva
-  },
-  data() {
-    return {
-      titulo: "AluraPic",
-      fotos: [],
-      filtro: ""
-    };
-  },
-  computed: {
-    fotosComFiltro() {
-      if (this.filtro) {
-        // filtra a lista, por enquanto vamos retornar uma lista em branco
-        let exp = new RegExp(this.filtro.trim(), "i");
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-        return [];
-      } else {
-        // se o campo estiver vazio, não filtramos, retornamos a lista
-        return this.fotos;
-      }
-    }
-  },
-
-  created() {
-    // alert("Criei o component");
-    let promise = this.$http.get("http://localhost:3000/v1/fotos");
-    promise
-      .then(res => res.json())
-      .then(fotos => ((this.fotos = fotos), err => console.log(err))); //this.fotos vem do data
-  }
-};
+export default {};
 </script>
 
 <style>
@@ -62,19 +11,5 @@ export default {
   font-family: Helvetica, sans-serif;
   width: 96%;
   margin: 0 auto;
-}
-.centralizado {
-  text-align: center;
-}
-.lista-fotos {
-  list-style: none;
-}
-.lista-fotos .lista-fotos-item {
-  display: inline-block;
-}
-
-.filtro {
-  display: block;
-  width: 100%;
 }
 </style>
---
+++ alurapic/src/components/home/home.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <h1 class="centralizado">{{titulo}}</h1>
+    <input
+      type="search"
+      class="filtro"
+      v-on:input="filtro=$event.target.value"
+      placeholder="filtre por parte do titulo"
+    />
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+        <meu-painel :titulo="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+        </meu-painel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Painel from "./shared/painel/Paniel.vue";
+import imagemResponsiva from "./shared/imagem-responsiva/imagemResponsiva.vue";
+export default {
+  components: {
+    "meu-painel": Painel,
+    "imagem-responsiva": imagemResponsiva
+  },
+  data() {
+    return {
+      titulo: "AluraPic",
+      fotos: [],
+      filtro: ""
+    };
+  },
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        // filtra a lista, por enquanto vamos retornar uma lista em branco
+        let exp = new RegExp(this.filtro.trim(), "i");
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+        return [];
+      } else {
+        // se o campo estiver vazio, não filtramos, retornamos a lista
+        return this.fotos;
+      }
+    }
+  },
+
+  created() {
+    // alert("Criei o component");
+    let promise = this.$http.get("http://localhost:3000/v1/fotos");
+    promise
+      .then(res => res.json())
+      .then(fotos => ((this.fotos = fotos), err => console.log(err))); //this.fotos vem do data
+  }
+};
+</script>
+
+<style>
+.centralizado {
+  text-align: center;
+}
+.lista-fotos {
+  list-style: none;
+}
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.filtro {
+  display: block;
+  width: 100%;
+}
+</style>
